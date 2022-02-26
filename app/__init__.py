6de6717fff035e53f96e5d07619678c67f5b23ca
Hillary-Kechem <hillarykechem92@gmail.com>
2022-02-26 12:39:39 +0300

    connect config with application

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from .config import Devconfig
+
+#initialising application
+app = Flask(__name__, instance_relative_config = True)
+
+#Setting up configuratiion 
+app.config.from_object(Devconfig)
+app.config.from__pyfile('config.py')
 
 
-app = Flask(__name__)
 from app import views
